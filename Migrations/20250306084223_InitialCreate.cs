0c550b12e896fa4c94705d628a30e1b74aca2abc
VisualBean <alwc@duos.dk>
2025-03-06 09:56:44 +0100

    inital commit

---
+++ Migrations/20250306084223_InitialCreate.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EF_Encapsulation_Starter.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Demons",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Age = table.Column<int>(type: "INTEGER", nullable: false),
+                    HasHorns = table.Column<bool>(type: "INTEGER", nullable: false),
+                    Name = table.Column<string>(type: "TEXT", nullable: false),
+                    PowerDamage = table.Column<int>(type: "INTEGER", nullable: false),
+                    PowerName = table.Column<string>(type: "TEXT", nullable: false),
+                    Underlings = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Demons", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Demons");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,68 @@
+﻿using System.Diagnostics;
+using Microsoft.EntityFrameworkCore;
+
+namespace EF_Encapsulation_Starter;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        using var db = new EncapsulationContext();
+        db.Database.EnsureCreated();
+
+        var demon = new Demon
+        {
+            Name = "Belzebob", // yes.. bob..
+            PowerDamage = 69,
+            PowerName = "FIREBALLZ"
+        };
+        
+        db.Demons.Add(demon);
+        db.SaveChanges();
+        
+        Console.WriteLine(demon);
+        Console.WriteLine(db.Demons.Count());
+    }
+}
+
+[DebuggerDisplay("Name: {Name}")]
+public class Demon
+{
+    public int Age { get; set; }
+
+    public bool HasHorns { get; set; }
+
+    public string Name { get; set; }
+    
+    public int PowerDamage { get; set; }
+
+    public string PowerName { get; set; }
+    
+    public List<string> Underlings { get; set; } = new List<string>();
+
+    public override string ToString()
+    {
+        return Name;
+    }
+}
+
+class EncapsulationContext : DbContext
+{
+    public DbSet<Demon> Demons { get; set; }
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Demon>(demon =>
+        {
+            demon.Property<int>("Id");
+            demon.HasKey("Id");
+        });
+
+        base.OnModelCreating(modelBuilder);
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlite("Data Source=demons.db");
+        base.OnConfiguring(optionsBuilder);
+    }
+}
